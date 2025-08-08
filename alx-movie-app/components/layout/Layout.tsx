acc42b149ff1b5178fd9fdea4f1e5a13e736751f
Anita Samuel <migist2@gmail.com>
2025-08-08 23:34:54 +0100

    movie interface

--- alx-movie-app/components/layout/Layout.tsx
+++ alx-movie-app/components/layout/Layout.tsx
@@ -1,5 +1,16 @@
-const Layout: React.FC = () => {
-  return <div></div>;
-};
+
+import Header from "./Header";
+import Footer from "./Footer";
+import { ComponentProps } from "@/interfaces";
+
+const Layout: React.FC<ComponentProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen">{children}</main>
+      <Footer />
+    </>
+  )
+}
 
 export default Layout;
