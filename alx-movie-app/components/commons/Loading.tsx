3c602627e85bb088713bfc88aacf04a544eacdc1
Anita Samuel <migist2@gmail.com>
2025-08-08 20:14:27 +0100

    movie

---
+++ alx-movie-app/components/commons/Loading.tsx
@@ -0,0 +1,5 @@
+const Loading: React.FC = () => {
+  return <div></div>;
+};
+
+export default Loading;
---
+++ alx-movie-app/components/commons/MovieCard.tsx
@@ -0,0 +1,5 @@
+const MovieCard: React.FC = () => {
+  return <div></div>;
+};
+
+export default MovieCard;
